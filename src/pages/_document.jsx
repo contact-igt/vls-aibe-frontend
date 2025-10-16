65d2ef57642bfd7184c26c0efd5560835379eea3
Veeravel-S <veeravel.igt@gmail.com>
2025-10-16 15:02:44 +0530

    responsive changes and icon added

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/assets/vls_logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
