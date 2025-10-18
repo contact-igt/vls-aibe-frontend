d7d11680e9ea0c02162931a2127838cc9d8c469e
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-10-18 19:23:34 +0530

    change test to live

--- src/common/Form/index.jsx
+++ src/common/Form/index.jsx
@@ -26,233 +26,129 @@ const Form = () => {
     }),
 
     onSubmit: async (values, { resetForm }) => {
-      // const resp = await fetch("/api/create-order", {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/json" },
-      //   body: JSON.stringify({ amount: 590 }),
-      // });
-
-      // const order = await resp.json();
-
-      // if (!resp.ok) {
-      //   console.error("Create order failed", order);
-      //   setisLoading(false);
-      //   router.replace("/error");
-      //   return;
-      // } else {
-      //   const options = {
-      //     key: process.env.REACT_APP_RAZORPAY_KEY_ID,
-      //     amount: order?.amount * 100,
-      //     currency: "INR",
-      //     name: values?.name,
-      //     order_id: order.id,
-      //     description: "₹500 + 18% Tax",
-
-      //     handler: async (response) => {
-      //       if (response?.razorpay_payment_id) {
-
-      //         setisLoading(true);
-
-      //         const ipResponse = await fetch(
-      //           "https://api.ipify.org?format=json"
-      //         );
-      //         const ipData = await ipResponse.json();
-
-      //         const formData = {
-      //           Name: values?.name,
-      //           Email: values?.email,
-      //           Mobile: `91${values?.mobile}`,
-      //           Amount: order?.amount / 100,
-      //           Razorpay_Transaction_Id: response?.razorpay_payment_id,
-      //           Payment_Status: "Paid",
-      //           ip_address: ipData.ip,
-      //           utm_source: localStorage.getItem("utm_source"),
-      //         };
-
-      //         const apiPayload = {
-      //           name: values?.name ? values?.name : "",
-      //           email: values?.email,
-      //           mobile: `91${values?.mobile}`,
-      //           amount: order?.amount / 100,
-      //           programm_start_date: "2025-10-31",
-      //           programm_end_date: "2025-11-02",
-      //           razorpay_order_id: response.razorpay_order_id
-      //             ? response.razorpay_order_id
-      //             : "",
-      //           razorpay_payment_id: response.razorpay_payment_id
-      //             ? response.razorpay_payment_id
-      //             : "",
-      //           razorpay_signature: response.razorpay_signature
-      //             ? response.razorpay_signature
-      //             : "",
-      //           payment_status: "paid",
-      //           captured: response.captured ? response.captured : "",
-      //           ip_address: ipData.ip,
-      //           utm_source: localStorage.getItem("utm_source"),
-      //         };
-
-      //         const whatsappPayload = {
-      //           phone: `91${values?.mobile}`,
-      //           name: values?.name,
-      //           amount: order?.amount / 100,
-      //           event_dates: "Oct 31, Nov 1 & 2",
-      //           event_date_time:
-      //             "Oct 31 → 5.00 PM – 9.00 PM IST  ,  Nov 1 & 2 → 9:30 AM – 1:00 PM IST",
-      //           platform: "Google Meet",
-      //           link_date: "Thursday Oct, 30",
-      //         };
-
-      //         useVlsAibeQuery(apiPayload)
-      //           .then(async (res) => {
-      //             const params = new URLSearchParams();
-      //             Object.keys(formData).forEach((key) => {
-      //               params.append(key, formData[key]);
-      //             });
-
-      //             const sheetRes = await fetch(
-      //               "https://script.google.com/macros/s/AKfycbyJg_xp9Duhv6AbPk4tcnIjHAqDJyxsGSmujNl7QnU_oMN29wr80g4ogIBG80nlPHY/exec",
-      //               {
-      //                 method: "POST",
-      //                 headers: {
-      //                   "Content-Type": "application/x-www-form-urlencoded",
-      //                 },
-      //                 body: params.toString(),
-      //               }
-      //             );
-      //             resetForm();
-      //             handleWhatsappMessage(whatsappPayload);
-      //             afterRegisterSuccessufull(formData);
-      //           })
-      //           .catch((err) => {
-      //             setisLoading(false);
-      //             resetForm();
-      //             router.push("/error");
-      //           });
-      //       } else {
-      //         router.push("/error");
-      //         setisLoading(false);
-      //       }
-      //     },
-      //     prefill: {
-      //       name: values?.name,
-      //       email: values?.email,
-      //       contact: values?.mobile,
-      //     },
-      //     theme: { color: "#b20a0a" },
-      //   };
-
-      //   const razor = new window.Razorpay(options);
-
-      //   razor.on("payment.failed", function () {
-      //     router.push("/error");
-      //     setisLoading(false);
-      //   });
-
-      //   razor.open();
-      // }
-
-      const options = {
-        key: "rzp_test_RFnGsVwVIs0EOi",
-        amount: 590 * 100,
-        currency: "INR",
-        name: values?.name,
-        // order_id: order.id,
-        description: "₹500 + 18% Tax",
-
-        handler: async (response) => {
-          if (response?.razorpay_payment_id) {
-            setisLoading(true);
-
-            const ipResponse = await fetch("https://api.ipify.org?format=json");
-            const ipData = await ipResponse.json();
-
-            const formData = {
-              Name: values?.name,
-              Email: values?.email,
-              Mobile: `91${values?.mobile}`,
-              Amount: 590,
-              Razorpay_Transaction_Id: response?.razorpay_payment_id,
-              Payment_Status: "Paid",
-              ip_address: ipData.ip,
-              utm_source: localStorage.getItem("utm_source"),
-            };
-
-            const apiPayload = {
-              name: values?.name ? values?.name : "",
-              email: values?.email,
-              mobile: `91${values?.mobile}`,
-              amount: 590,
-              programm_start_date: "2025-10-31",
-              programm_end_date: "2025-11-02",
-              razorpay_order_id: "",
-              razorpay_payment_id: response.razorpay_payment_id
-                ? response.razorpay_payment_id
-                : "",
-              razorpay_signature: "",
-              payment_status: "paid",
-              captured: "",
-              ip_address: ipData.ip,
-              utm_source: localStorage.getItem("utm_source"),
-            };
-
-            const whatsappPayload = {
-              phone: `91${values?.mobile}`,
-              name: values?.name,
-              amount: 590,
-              event_dates: "Oct 31, Nov 1 & 2",
-              event_date_time:
-                "Oct 31 → 5.00 PM – 9.00 PM IST  ,  Nov 1 & 2 → 9:30 AM – 1:00 PM IST",
-              platform: "Google Meet",
-              link_date: "Thursday Oct, 30",
-            };
-
-            useVlsAibeQuery(apiPayload)
-              .then((res) => {
-                // const params = new URLSearchParams();
-                // Object.keys(formData).forEach((key) => {
-                //   params.append(key, formData[key]);
-                // });
-
-                // const sheetRes = await fetch(
-                //   "https://script.google.com/macros/s/AKfycbyJg_xp9Duhv6AbPk4tcnIjHAqDJyxsGSmujNl7QnU_oMN29wr80g4ogIBG80nlPHY/exec",
-                //   {
-                //     method: "POST",
-                //     headers: {
-                //       "Content-Type": "application/x-www-form-urlencoded",
-                //     },
-                //     body: params.toString(),
-                //   }
-                // );
-                resetForm();
-                handleWhatsappMessage(whatsappPayload);
-                afterRegisterSuccessufull(formData);
-              })
-              .catch((err) => {
-                setisLoading(false);
-                resetForm();
-                router.push("/error");
-              });
-          } else {
-            router.push("/error");
-            setisLoading(false);
-          }
-        },
-        prefill: {
-          name: values?.name,
-          email: values?.email,
-          contact: values?.mobile,
-        },
-        theme: { color: "#b20a0a" },
-      };
-
-      const razor = new window.Razorpay(options);
-
-      razor.on("payment.failed", function () {
-        router.push("/error");
-        setisLoading(false);
+      const resp = await fetch("/api/create-order", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ amount: 590 }),
       });
 
-      razor.open();
+      const order = await resp.json();
+
+      if (!resp.ok) {
+        console.error("Create order failed", order);
+        setisLoading(false);
+        router.replace("/error");
+        return;
+      } else {
+        const options = {
+          key: process.env.REACT_APP_RAZORPAY_KEY_ID,
+          amount: order?.amount * 100,
+          currency: "INR",
+          name: values?.name,
+          order_id: order.id,
+          description: "₹500 + 18% Tax",
+
+          handler: async (response) => {
+            if (response?.razorpay_payment_id) {
+              setisLoading(true);
+
+              const ipResponse = await fetch(
+                "https://api.ipify.org?format=json"
+              );
+              const ipData = await ipResponse.json();
+
+              const formData = {
+                Name: values?.name,
+                Email: values?.email,
+                Mobile: `91${values?.mobile}`,
+                Amount: order?.amount / 100,
+                Razorpay_Transaction_Id: response?.razorpay_payment_id,
+                Payment_Status: "Paid",
+                ip_address: ipData.ip,
+                utm_source: localStorage.getItem("utm_source"),
+              };
+
+              const apiPayload = {
+                name: values?.name ? values?.name : "",
+                email: values?.email,
+                mobile: `91${values?.mobile}`,
+                amount: order?.amount / 100,
+                programm_start_date: "2025-10-31",
+                programm_end_date: "2025-11-02",
+                razorpay_order_id: response.razorpay_order_id
+                  ? response.razorpay_order_id
+                  : "",
+                razorpay_payment_id: response.razorpay_payment_id
+                  ? response.razorpay_payment_id
+                  : "",
+                razorpay_signature: response.razorpay_signature
+                  ? response.razorpay_signature
+                  : "",
+                payment_status: "paid",
+                captured: response.captured ? response.captured : "",
+                ip_address: ipData.ip,
+                utm_source: localStorage.getItem("utm_source"),
+              };
+
+              const whatsappPayload = {
+                phone: `91${values?.mobile}`,
+                name: values?.name,
+                amount: order?.amount / 100,
+                event_dates: "Oct 31, Nov 1 & 2",
+                event_date_time:
+                  "Oct 31 → 5.00 PM – 9.00 PM IST  ,  Nov 1 & 2 → 9:30 AM – 1:00 PM IST",
+                platform: "Google Meet",
+                link_date: "Thursday Oct, 30",
+              };
+
+              useVlsAibeQuery(apiPayload)
+                .then((res) => {
+                  // const params = new URLSearchParams();
+                  // Object.keys(formData).forEach((key) => {
+                  //   params.append(key, formData[key]);
+                  // });
+
+                  // const sheetRes = await fetch(
+                  //   "https://script.google.com/macros/s/AKfycbyJg_xp9Duhv6AbPk4tcnIjHAqDJyxsGSmujNl7QnU_oMN29wr80g4ogIBG80nlPHY/exec",
+                  //   {
+                  //     method: "POST",
+                  //     headers: {
+                  //       "Content-Type": "application/x-www-form-urlencoded",
+                  //     },
+                  //     body: params.toString(),
+                  //   }
+                  // );
+                  resetForm();
+                  handleWhatsappMessage(whatsappPayload);
+                  afterRegisterSuccessufull(formData);
+                })
+                .catch((err) => {
+                  setisLoading(false);
+                  resetForm();
+                  router.push("/error");
+                });
+            } else {
+              router.push("/error");
+              setisLoading(false);
+            }
+          },
+          prefill: {
+            name: values?.name,
+            email: values?.email,
+            contact: values?.mobile,
+          },
+          theme: { color: "#b20a0a" },
+        };
+
+        const razor = new window.Razorpay(options);
+
+        razor.on("payment.failed", function () {
+          router.push("/error");
+          setisLoading(false);
+        });
+
+        razor.open();
+      }
     },
   });
 
@@ -272,7 +168,7 @@ const Form = () => {
 
   const afterRegisterSuccessufull = (formData) => {
     setTimeout(() => {
-      // router.replace("/thank-you");
+      // router.push("/thank-you");
       window.location.href = "/thank-you";
       localStorage.setItem("PaymentDeatls", JSON.stringify(formData));
       setisLoading(false);
