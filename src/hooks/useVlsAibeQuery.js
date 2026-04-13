76c550737976a7121fd92d88b20877028c599d30
tariq <tariq@salina.group>
2026-04-13 17:18:16 +0530

    added api

--- src/hooks/useVlsAibeQuery.js
+++ src/hooks/useVlsAibeQuery.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
-const VLSAIBE = "/vls-law-aibe";
+// const VLSAIBE = "/vls-law-aibe";
 
-const APISERVER =
-  process.env.NEXT_PUBLIC_API_SERVER === "production"
-    ? process.env.NEXT_PUBLIC_PRODUCTION_API_URL
-    : process.env.NEXT_PUBLIC_STAGE_API_URL === "stage"
-      ? process.env.NEXT_PUBLIC_STAGE_API_URL
-      : process.env.NEXT_PUBLIC_LOCALHOST_API_URL;
+// const APISERVER =
+//   process.env.NEXT_PUBLIC_API_SERVER === "production"
+//     ? process.env.NEXT_PUBLIC_PRODUCTION_API_URL
+//     : process.env.NEXT_PUBLIC_STAGE_API_URL === "stage"
+//       ? process.env.NEXT_PUBLIC_STAGE_API_URL
+//       : process.env.NEXT_PUBLIC_LOCALHOST_API_URL;
 
 export const useVlsAibeQuery = async (data) => {
   try {
-    const response = await axios.post(`${APISERVER}${VLSAIBE}`, data);
+    const response = await axios.post(
+      `https://stageapi.invictusglobaltech.com/api/v1/vls-law-aibe`,
+      data,
+    );
     return response.data;
   } catch (error) {
-    console.error("useVlsAibeQuery API Error:", error?.response?.data || error.message);
+    console.error(
+      "useVlsAibeQuery API Error:",
+      error?.response?.data || error.message,
+    );
     return { success: false, error: error?.response?.data || error.message };
   }
 };
-
